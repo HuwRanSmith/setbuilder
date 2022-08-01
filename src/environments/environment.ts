abc0f321bfa5380f71032fb9a8ae233786d5043f
Huw Smith <has840@cs.bham.ac.uk>
2022-08-01 12:12:30 +0100

    Added @angular/fire package

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'setbuilder-a706f',
+    appId: '1:341866267460:web:d3acf914b705f49286cd11',
+    storageBucket: 'setbuilder-a706f.appspot.com',
+    apiKey: 'AIzaSyBboEbThi6IfEB6NBOSCnbEaixQRgyzHgw',
+    authDomain: 'setbuilder-a706f.firebaseapp.com',
+    messagingSenderId: '341866267460',
+    measurementId: 'G-XEW4SPC941',
+  },
   production: false
 };
 
