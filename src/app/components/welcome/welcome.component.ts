526380add1d4a6612cf2e72b414a41be0e358d26
Huw Smith <has840@cs.bham.ac.uk>
2022-08-29 22:13:13 +0100

    made it look pretty

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Auth, onAuthStateChanged } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.css']
+  styleUrls: ['./welcome.component.css'],
 })
 export class WelcomeComponent implements OnInit {
+  user$: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private auth: Auth) {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        this.user$ = user;
+      } else {
+        this.user$ = null;
+      }
+    });
   }
 
+  ngOnInit(): void {}
 }
